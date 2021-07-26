6117b0331c1736d5c3868572a5ca95b8612ae085
Harshit Gupta <harshit79090@gmail.com>
2021-07-26 17:57:15 +0530

    Added firebase cloud messagin and realtime database

--- app/src/main/java/com/viewmodel/chatapp/activities/SignInActivity.kt
+++ app/src/main/java/com/viewmodel/chatapp/activities/SignInActivity.kt
@@ -3,8 +3,13 @@ package com.viewmodel.chatapp.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import com.viewmodel.chatapp.R
 import com.viewmodel.chatapp.databinding.ActivitySignInBinding
+import java.util.*
+import kotlin.collections.HashMap
 
 class SignInActivity : AppCompatActivity() {
     lateinit var binding: ActivitySignInBinding
@@ -14,6 +19,27 @@ class SignInActivity : AppCompatActivity() {
         setContentView(binding.root)
         binding.tvCreateNewAccount.setOnClickListener {
             startActivity(Intent(applicationContext,SignUpActivity::class.java))
+
+
         }
+        binding.btnSignIn.setOnClickListener{
+            addDatatoFirestore()
+        }
+    }
+
+    private fun addDatatoFirestore() {
+        val database = FirebaseFirestore.getInstance()
+        val data = HashMap<String, Any>()
+        data.put("first_name","Harshit")
+        data.put("last_name","Gupta")
+        Log.d("dataii","${data["last_name"]}")
+        database.collection("users")
+            .add(data)
+            .addOnSuccessListener {
+                Toast.makeText(this,"Data Inserted",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener{
+                Toast.makeText(this,"$it",Toast.LENGTH_SHORT).show()
+            }
     }
 }
---
+++ app/src/main/java/com/viewmodel/chatapp/firebase/MessagingService.kt
@@ -0,0 +1,18 @@
+package com.viewmodel.chatapp.firebase
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+
+class MessagingService: FirebaseMessagingService() {
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        super.onMessageReceived(remoteMessage)
+
+    }
+
+}
